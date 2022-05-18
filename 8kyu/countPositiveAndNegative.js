e1c30dc70c90eddc2d269807228165ab80115d1b
Chrmejia95 <97995687+Chrmejia95@users.noreply.github.com>
2022-05-18 08:06:31 -0500

    Update countPositiveAndNegative.js

--- 8kyu/countPositiveAndNegative.js
+++ 8kyu/countPositiveAndNegative.js
@@ -1,10 +1,10 @@
 function countPositivesSumNegatives(input) {
-    let positiveNum = 0
-    let negativeNum = 0
-    if( input === null || input === 0){
-      return []
-    }else{
-      input.forEach((num) => num > 0 ? positiveNum++ : negativeNum += num)
-       }
-  return[positiveNum,negativeNum]
+    let positiveNums = 0;
+    let negativeNums = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    } else {
+      input.forEach((num) => num > 0 ? positiveNums++ : negativeNums += num);
+    }
+    return [positiveNums , negativeNums];
 }
