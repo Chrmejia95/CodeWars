2d2a86a42bbac7e829e7e1d9dd3a7a16b57c3e8e
Chris Mejia <Christophermejia1026@gmail.com>
2022-05-23 12:40:31 -0500

    5/23/22

---
+++ 8kyu/getPlanetNameById.js
@@ -0,0 +1,20 @@
+function getPlanetName(planetNumber){
+    if(planetNumber == '1'){
+      return 'Mercury'
+    }else if(planetNumber == '2'){
+      return 'Venus'
+    }else if(planetNumber == '3'){
+      return 'Earth'
+    }else if(planetNumber == '4'){
+      return 'Mars'
+    }else if(planetNumber == '5'){
+      return 'Jupiter'
+    }else if(planetNumber == '6'){
+      return 'Saturn'
+    }else if(planetNumber == '7'){
+      return 'Uranus'
+    }else if(planetNumber == '8'){
+      return 'Neptune'
+    }
+     
+   }
