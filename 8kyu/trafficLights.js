4e380b1cdedec7260315d4f6d71a1f252359a36c
Chris Mejia <Christophermejia1026@gmail.com>
2022-05-24 08:07:28 -0500

    5/24/22

---
+++ 8kyu/trafficLights.js
@@ -0,0 +1,10 @@
+function updateLight(current) {
+    if(current == "red"){
+      return "green"
+    }else if(current == "green"){
+      return "yellow"
+    }else if(current == "yellow"){
+      return "red"
+    }
+  
+  }
